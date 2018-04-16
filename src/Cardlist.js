5065147cd2ea635e2d1d0dd45b15a29bf2c31d5c
Brandan Richardson <brandanr@gmail.com>
2018-04-16 14:26:20 -0700

    reconfigured Cardlist component

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -2,19 +2,22 @@ import React from 'react';
 import Card from'./Card';
 
 const Cardlist = ({ robots }) => {
-    const cardArray = robots.map((user, i) => {
-        return (<Card 
-            key={i}
-            id={robots[i].id}
-            name={robots[i].name} 
-            email={robots[i].email} />
-        );
-    })
-return (
-    <div>
-        {cardArray}
+    return (
+        <div>
+          {
+        robots.map((user, i) => {
+            return (
+                <Card 
+                    key={i}
+                    id={robots[i].id}
+                    name={robots[i].name} 
+                    email={robots[i].email} 
+                    />
+                );
+            })
+        }
     </div>
-);
+    );
 }
 
 export default Cardlist;
